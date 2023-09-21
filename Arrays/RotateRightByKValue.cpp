e5f730aba04d88b76acf956c0fd390c09df7ebdd
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2023-09-21 15:23:14 +0530

    Create RotateRightByKValue.cpp

---
+++ Arrays/RotateRightByKValue.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotateRightByK(int arr[], int n, int k) {
+  k = k%n;
+
+  for(int i=0; i<n; i++) {
+    if(i<k) {
+      cout<<arr[n+i-k]<<" ";
+    }
+    else {
+      cout<<arr[i-k]<<" ";
+    }
+  }
+  
+}
+
+int main() {
+  int n;
+  cin>>n;
+
+  int arr[n];
+  for(int i=0; i<n; i++) {
+    cin>>arr[i];
+  }
+
+  int k;
+  cin>>k;
+
+  rotateRightByK(arr, n, k);
+}
