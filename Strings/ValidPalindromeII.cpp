05ac162d3f9812c836aff05d547273efe11acbcc
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2025-04-07 01:51:22 +0530

    Create ValidPalindromeII.cpp

---
+++ Strings/ValidPalindromeII.cpp
@@ -0,0 +1,38 @@
+// Valid Palindrome II   ( Leetcode : 680 )
+
+class Solution {
+public:
+
+    bool checkPalindrome(string s, int i, int j) {
+        while(i < j) {
+            if(s[i] == s[j]) {
+                i++, j--;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i < j) {
+            if(s[i] == s[j]) {
+                i++, j--;
+            }
+            else {
+                //remove ith element
+                bool ans1 = checkPalindrome(s, i+1, j);
+
+                //remove jth element
+                bool ans2 = checkPalindrome(s, i, j-1);
+
+                return ans1 || ans2;
+            }
+        }
+        return true;
+    }
+};
