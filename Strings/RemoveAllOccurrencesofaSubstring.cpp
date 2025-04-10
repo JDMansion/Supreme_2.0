bab396589490ec0f7b236b803f262bf533630e39
Jaideep Sharma <40294495+JDMansion@users.noreply.github.com>
2025-04-10 23:06:25 +0530

    Update RemoveAllOccurrencesofaSubstring.cpp

--- Strings/RemoveAllOccurrencesofaSubstring.cpp
+++ Strings/RemoveAllOccurrencesofaSubstring.cpp
@@ -9,3 +9,23 @@ public:
         return s;
     }
 };
+
+// 2nd Method : 
+// Here, we are using string as a stack to insert each character from a string, and check at the last of ans string if it matches with 
+// the part, then we delete those and continue till the end of string
+// class Solution {
+// public:
+//     string removeOccurrences(string s, string part) {
+//         string ans;
+//         for(auto a : s) {
+//             ans.push_back(a);
+//             if(ans.size() >= part.size() && ans.substr(ans.size()-part.size())==part) {
+//                 int m = part.size();
+//                 while(m--) {
+//                     ans.pop_back();
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
